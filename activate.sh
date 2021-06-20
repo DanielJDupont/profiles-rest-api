8aac90cca0d1c92c95e5922702d004680573f0a0
DanielJDupont <ddupont2@uwo.ca>
2021-06-19 23:30:50 -0400

    removed unwanted nesting

--- activate.sh
+++ activate.sh
@@ -1,2 +1,5 @@
 # Activate the python virutal environment in .pyvenv.
 source .pyvenv/bin/activate
+
+# Note that you will need to use python manage.py instead of django-admin once you have created your project.
+# You can create new apps using the python manage.py commands.
